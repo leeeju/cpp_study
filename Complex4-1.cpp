455f2e2169e77553a844ce3ab70607395a078c50
이주현 <84003327+leeeju@users.noreply.github.com>
2021-05-13 21:24:31 +0900

    Create Complex4-1.cpp

---
+++ Complex4-1.cpp
@@ -0,0 +1,67 @@
+#include "complex.h"
+
+Complex::Complex()
+{
+    this -> re = 0.0;
+    this -> im = 0.0;
+}
+
+Complex::Complex(double re)
+{
+    this -> re = re;
+    this -> im = 0.0;
+}
+
+Complex::Complex(double re, double im)
+{
+    this -> re = re;
+    this -> re = re;
+}
+
+Complex::Complex(const Complex& rhs)
+{
+    this -> re = rhs.re;
+    this -> im = rhs.im;
+}
+
+Complex:: ~ Complex()
+{
+
+}
+
+Complex::operator = (const Complex& rhx)
+{
+    this -> re = rhs.re;
+    this -> re = rhs.re;
+}
+
+bool Complex::operator==(const Complex& rhx)
+{
+    //if (this -> re == rhs.re && this -> im == rhs.im )
+    //    return false;
+    //    else   
+    //    return false;
+    
+     return (this -> re == rhs.re && this -> im == rhs.im );
+}
+
+double Complex:: real()
+{
+    return this -> re;
+}
+
+double Complex:: imag()
+{
+    return this -> im;
+}
+void Complex::real(double re)
+{
+    this->re=re;
+}
+
+void Complex::imag(double im)
+{
+    this->im=im;
+}
+
+2021.05.13 Complex 인자
